10bed6fb180150c02908c32f8fa3a3ea8db9b7d1
winglechen@aliyun.com <winglechen@gmail.com>
2025-05-08 10:33:42 +0800

    feature: add method encode/decode to JSONUtil

--- common/src/main/java/cn/coderule/common/util/lang/string/JSONUtil.java
+++ common/src/main/java/cn/coderule/common/util/lang/string/JSONUtil.java
@@ -4,6 +4,9 @@ import cn.coderule.common.util.lang.StringUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import lombok.NonNull;
 import cn.coderule.common.util.lang.collection.ArrayUtil;
 
@@ -18,6 +21,7 @@ import java.util.Map;
  * @since 2020/2/26 1:00 下午
  **/
 public class JSONUtil {
+    private final static Charset CHARSET_UTF8 = StandardCharsets.UTF_8;
     public static final String EMPTY_JSON = "{}";
     public static final String KEY_SEPARATOR = ".";
 
@@ -139,5 +143,50 @@ public class JSONUtil {
 
         return result;
     }
+
+    public static byte[] encode(final Object obj) {
+        if (obj == null) {
+            return null;
+        }
+        final String json = toJson(obj);
+        return json.getBytes(CHARSET_UTF8);
+    }
+
+    public static byte[] encode(final Object obj, JSONWriter.Feature... features) {
+        if (obj == null) {
+            return null;
+        }
+
+        String json = JSON.toJSONString(obj, features);
+        return json.getBytes(CHARSET_UTF8);
+    }
+
+    public static String toJson(final Object obj) {
+        return JSON.toJSONString(obj);
+    }
+
+    public static <T> T decode(final byte[] data, Class<T> classOfT) {
+        if (data == null) {
+            return null;
+        }
+        return fromJson(data, classOfT);
+    }
+
+    public static <T> List<T> decodeList(final byte[] data, Class<T> classOfT) {
+        if (data == null) {
+            return null;
+        }
+        String json = new String(data, CHARSET_UTF8);
+        return JSON.parseArray(json, classOfT);
+    }
+
+    public static <T> T fromJson(String json, Class<T> classOfT) {
+        return JSON.parseObject(json, classOfT);
+    }
+
+    private static <T> T fromJson(byte[] data, Class<T> classOfT) {
+        return JSON.parseObject(data, classOfT);
+    }
+
 
 }
